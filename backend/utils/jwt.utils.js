a2710a8c10c5b9ad3f9a77ef8cbbe014b0cb1a7f
Yosef Kovan <154745088+YosefKovan@users.noreply.github.com>
2026-03-17 10:11:13 +0200

    adding the jwt utils file that will handle the jwt functionality

---
+++ backend/utils/jwt.utils.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+import AppError from "../errors/app.errors";
+
+const JWT_SECRET = process.env.JWY_SECRET;
+
+const JWT_EXPIRES = process.env.JWT_EXPIRES || '24h'
+
+export function signToken(payload){
+    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES});
+    return token;
+}
+
+export function validateToken(token){
+    
+    try{
+        const payload = jwt.verify(token, JWT_SECRET);
+        return payload;
+    }catch(error){
+        throw new AppError(401,  `Unauthorized - ${error.message}`);
+    }
+}
